ee4b45d392f8c0944a9547ba3ff867c98212aa79
Jacob Grass <jacob.grass.59@astradian.com>
2025-01-08 12:05:17 -0700

    change executables to *.out so they are left behind by .gitignore

--- fortran_lang_examples/hello_world/hello.f90
+++ fortran_lang_examples/hello_world/hello.f90
@@ -1,13 +1,13 @@
 ! Having saved your program to hello.f90, compile at the command line with:
 !
-! $> gfortran hello.f90 -o hello
+! $> gfortran hello.f90 -o hello.out
 !
 !     .f90 is the standard file extension for modern Fortran source files. The 90 refers to the first
 !     modern Fortran standard in 1990.
 !
 ! To run your compiled program:
 !
-! $> ./hello
+! $> ./hello.out
 ! Hello, World!
 !
 ! Congratulations, you’ve written, compiled and run your first Fortran program! In the next part of
