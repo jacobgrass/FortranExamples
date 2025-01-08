52361d6147ae6714460640e716ee5f890d9ffdca
Jacob Grass <jacob.grass.59@astradian.com>
2025-01-08 12:02:28 -0700

    add hello world

---
+++ fortran_lang_examples/hello_world/hello.f90
@@ -0,0 +1,19 @@
+! Having saved your program to hello.f90, compile at the command line with:
+!
+! $> gfortran hello.f90 -o hello
+!
+!     .f90 is the standard file extension for modern Fortran source files. The 90 refers to the first
+!     modern Fortran standard in 1990.
+!
+! To run your compiled program:
+!
+! $> ./hello
+! Hello, World!
+!
+! Congratulations, you’ve written, compiled and run your first Fortran program! In the next part of
+! this tutorial, we will introduce variables for storing data.
+
+program hello
+   ! This is a comment line; it is ignored by the compiler
+   print *, 'Hello, World!'
+end program hello
